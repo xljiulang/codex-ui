26681128451a90a0fc888b0392a3959ecf005b43
陈国伟 <366193849@qq.com>
2026-08-08 23:23:34 +0800

    feat: 头部品牌小 Logo 与胶囊新建按钮
    - 品牌前增加橙色描边六边形 Logo
    - 新建对话改为胶囊主按钮(渐变+文字+光晕), 从普通图标中突出

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -35,6 +35,11 @@ function onHistory() {
 <template>
   <header class="app-header">
     <div class="brand">
+      <span class="brand-logo">
+        <svg viewBox="0 0 24 24">
+          <path d="M12 2l8.66 5v10L12 22l-8.66-5V7z" />
+        </svg>
+      </span>
       <span class="brand-name">CODEX</span>
       <button
         v-if="cwd"
@@ -75,10 +80,11 @@ function onHistory() {
           />
         </svg>
       </button>
-      <button class="icon-btn" title="新建对话" @click="onNewChat()">
+      <button class="icon-btn newchat-btn" title="新建对话" @click="onNewChat()">
         <svg viewBox="0 0 24 24">
           <path d="M11 5h2v6h6v2h-6v6h-2v-6H5v-2h6z" />
         </svg>
+        <span>新建对话</span>
       </button>
     </div>
   </header>
